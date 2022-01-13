e7fd8c96a07664650955c167071c96f84de31964
kim do yeon <dy9601@naver.com>
2022-01-13 16:30:04 +0900

    주문완료 페이지 시작

--- src/main/java/com/guddi/shop/controller/CartController.java
+++ src/main/java/com/guddi/shop/controller/CartController.java
@@ -1,5 +1,31 @@
 package com.guddi.shop.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.guddi.shop.service.CartService;
+
+@Controller
 public class CartController {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired CartService service;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model, HttpSession session) {
+
+		logger.info("홈페이지 요청");					
+		
+
+		return "index";
+	}
 
 }
--- src/main/java/com/guddi/shop/service/CartService.java
+++ src/main/java/com/guddi/shop/service/CartService.java
@@ -1,5 +1,17 @@
 package com.guddi.shop.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guddi.shop.dao.CartDao;
+
+@Service
 public class CartService {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired CartDao dao;
 
 }
