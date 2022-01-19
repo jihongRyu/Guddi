aa072fb332ffef97fbd0bdfd51532c26fd3ccd68
song seung hyuck <tmdgur1541@naver.com>
2022-01-19 10:37:41 +0900

    문의타입 변경 기능 오류 수정

--- src/main/java/com/guddi/shop/controller/MemberController.java
+++ src/main/java/com/guddi/shop/controller/MemberController.java
@@ -454,6 +454,7 @@ public class MemberController {
 						logger.info("session3 실행");
 						session.setAttribute("mem_flg", dto.getMem_flg());
 						logger.info("session에서 mem_flg가져오기");
+						session.setAttribute("u_idx", dto.getIdx());
 					
 					//logger.info("cartCnt 반환");
 
@@ -467,6 +468,7 @@ public class MemberController {
 					session.setAttribute("mem_flg", dto.getMem_flg());
 					logger.info("session에서 mem_flg가져오기");
 					session.setAttribute("u_idx", dto.getIdx());
+					
 				}
 				
 			}catch(Exception e){
